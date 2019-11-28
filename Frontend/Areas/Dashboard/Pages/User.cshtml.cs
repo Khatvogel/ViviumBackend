650e1e0a9a9b14444d31fb9aca3aec77267d5d45
alex <alex.chorak@hva.nl>
2019-11-28 14:07:20 +0100

    html helper toegevoegd en layout

---
+++ Frontend/Areas/Dashboard/Pages/User.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontend.Areas.Dashboard.Pages
+{
+    public class UserModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ Frontend/HtmlHelper/MenuItemHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Frontend.HtmlHelper
+{
+    public static class MenuItemHelper
+    {
+        public static string IsSelected(this IHtmlHelper htmlHelper, string area, string cssClass = "active")
+        {
+
+            var currentArea = htmlHelper.ViewContext.RouteData.Values["area"] as string;
+
+            if (currentArea == null) return string.Empty;
+
+            IEnumerable<string> acceptedActions = (area ?? currentArea).Split(',');
+
+            return acceptedActions.Contains(currentArea) ? cssClass : string.Empty;
+        }
+    }
+
+}
