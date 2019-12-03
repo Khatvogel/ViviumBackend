e6baaaa2183b87e7a93cbec9f223ce991d5b1ea2
alexxzan <alexander_wunderlich@msn.com>
2019-12-03 18:38:37 +0100

    timer toegevoegd

--- Frontend/Areas/Dashboard/Pages/Index.cshtml.cs
+++ Frontend/Areas/Dashboard/Pages/Index.cshtml.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Threading.Tasks;
+using Backend.Interfaces.Repositories;
+using Frontend.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Frontend.Areas.Dashboard.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IAttemptDeviceRepository _repository;
+
+        public IndexModel(ILogger<IndexModel> logger, IAttemptDeviceRepository repository)
         {
-            
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [BindProperty] public DashboardViewModel DashboardViewModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var list = await _repository.GetListAsync();
+            DashboardViewModel = new DashboardViewModel();
+            return Page();
         }
     }
 }
---
+++ Frontend/wwwroot/material-dashboard/assets/js/dashboard-index/dashboard-timer.js
@@ -0,0 +1,38 @@
+﻿let started = false;
+let totalSeconds = -1;
+let gameStatusHeader = $("#game-status-header");
+
+gameStatusHeader.click(function () {
+    $("#start-new-game-modal").modal();
+});
+
+$("#new-game-button").click(function () {
+    if (!started) {
+        countTimer();
+        window.myInterval = setInterval(countTimer, 1000);       
+        gameStatusHeader.removeClass("card-header card-header-success");
+        gameStatusHeader.addClass("card-header card-header-danger");
+        $("#modal-body-text").html("Wilt u het spel beëindigen?");
+        started = true;
+    } else {
+        if (window.myInterval !== undefined && window.myInterval !== 'undefined') {
+            window.clearInterval(window.myInterval);
+        }
+        totalSeconds = 0;
+        gameStatusHeader.removeClass("card-header card-header-danger");
+        gameStatusHeader.addClass("card-header card-header-success");
+        gameStatusHeader.html("<h2>Start a new game!</h2>");
+        started = false;
+    }
+});
+
+function countTimer() {
+    ++totalSeconds;
+    let hour = Math.floor(totalSeconds / 3600);
+    let minute = Math.floor((totalSeconds - hour * 3600) / 60);
+    let seconds = totalSeconds - (hour * 3600 + minute * 60);
+
+    document.getElementById("game-status-header").innerHTML
+        = "<h2>Total time: " + hour + ":" + minute + ":" + seconds + "</h2>";
+}
+
